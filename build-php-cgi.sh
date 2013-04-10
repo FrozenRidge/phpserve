6ec4fa718f3b6c7825fc47390a8b62a17931566a
Niall O'Higgins <niallo@unworkable.org>
2013-04-10 16:23:32 -0700

    add script to build php-cgi on os x

---
+++ build-php-cgi.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+php_install_dir=$(pwd)/php-dir
+conf_dir=$(pwd)
+tar xfz php-5.4.13.tar.gz
+cd php-5.4.13
+./configure --prefix=$php_install_dir \
+        --with-regex=php \
+        --enable-cgi \
+        --disable-rpath \
+        --disable-static \
+        --with-pic \
+        --with-zlib \
+        --with-curl \
+        --with-curlwrappers \
+        --enable-ftp \
+        --enable-zip \
+        --with-openssl \
+        --enable-mbstring  \
+        --enable-inline-optimization \
+        --disable-debug \
+        --with-config-file-path=$conf_dir \
+        && make && make install
